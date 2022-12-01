53d64274fca8d25884ac1c22be8587b1251f6afc
Michael Limiero <michael.limiero@datawalk.com>
2022-12-01 00:25:47 -0500

    Cleanup

--- day01.py
+++ day01.py
@@ -1,31 +1,21 @@
 import fileinput
 
 
-test_input = """1000
-2000
-3000
+def solve(lines):
+    groups = [[]]
+    for line in lines:
+        if not line.strip():
+            groups.append([])
+        else:
+            groups[-1].append(line)
 
-4000
+    sums = [sum(map(int, group)) for group in groups]
+    return (max(sums), sum(list(sorted(sums))[-3:]))
 
-5000
-6000
+def test_solve():
+    assert solve(fileinput.input("test01.txt")) == (24000, 45000)
 
-7000
-8000
-9000
-
-10000"""
-
-lines = test_input.split('\n')
-groups = [[]]
-for line in fileinput.input():
-    if not line.strip():
-        groups.append([])
-    else:
-        groups[-1].append(line)
-
-sums = [sum(map(int, group)) for group in groups]
-print(sums)
-print(max(sums))
-print((list(sorted(sums))[-3:]))
-print(sum(list(sorted(sums))[-3:]))
+if __name__ == '__main__':
+    part1, part2 = solve(fileinput.input())
+    print(part1)
+    print(part2)
