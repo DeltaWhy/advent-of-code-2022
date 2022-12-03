d5cb7aa7ec1d51ee5ac76017d701984967912334
Michael Limiero <michael.limiero@datawalk.com>
2022-12-03 00:16:11 -0500

    Day 3

--- template.py
+++ template.py
@@ -2,20 +2,27 @@ import fileinput
 import itertools
 
 
-def solve(lines):
-    lines1, lines2 = itertools.tee(lines)
-    part1 = None
-    part2 = None
-    for line in lines1:
-        pass
-    for line in lines2:
-        pass
-    return part1, part2
+TEST_FILE = "testXX.txt"
 
-def test_solve():
-    assert solve(fileinput.input("testXX.txt")) == (None, None)
+
+def solve_part1(lines):
+    for line in lines:
+        pass
+
+def test_solve_part1():
+    assert solve_part1(fileinput.input(TEST_FILE)) == None
+
+def solve_part2(lines):
+    for line in lines:
+        pass
+
+def test_solve_part2():
+    assert solve_part2(fileinput.input(TEST_FILE)) == None
+
 
 if __name__ == '__main__':
-    part1, part2 = solve(fileinput.input())
+    lines1, lines2 = itertools.tee(fileinput.input())
+    part1 = solve_part1(lines1)
+    part2 = solve_part2(lines2)
     print(part1)
     print(part2)
