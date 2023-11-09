d546f8bf3986130320ab7afb4717a627a78f8142
Michael Limiero <mike5713@gmail.com>
2023-11-08 19:45:22 -0500

    Day 18 part 2

--- day18.py
+++ day18.py
@@ -21,13 +21,41 @@ def test_parse():
     res = parse(fileinput.input(TEST_FILE))
     print(res)
 
+class Grid3(list):
+    def __init__(self, max_x, max_y, max_z):
+        self.max_x = max_x
+        self.max_y = max_y
+        self.max_z = max_z
+        super().__init__([False] * (max_x * max_y * max_z))
+    
+    def __getitem__(self, coord):
+        return super().__getitem__(coord[0] + coord[1] * self.max_x + coord[2] * self.max_x * self.max_y)
+
+    def __setitem__(self, coord, value):
+        super().__setitem__(coord[0] + coord[1] * self.max_x + coord[2] * self.max_x * self.max_y, value)
+
+    def clone(self):
+        res = Grid3.__new__(Grid3)
+        res.max_x = self.max_x
+        res.max_y = self.max_y
+        res.max_z = self.max_z
+        list.__init__(res, self)
+        return res
+
 def solve_part1(data):
     res = 0
+    max_size = max((max(vec) for vec in data)) + 1
+    grid = Grid3(max_size, max_size, max_size)
+    for cur in data:
+        grid[cur] = True
     for cur in data:
         sides = 6
-        for other in data:
-            if other != cur and abs(cur - other).manhattan() == 1:
+        for other in cur.cneighbors():
+            if max(other) < max_size and grid[other]:
                 sides -= 1
+        # for other in data:
+        #     if other != cur and abs(cur - other).manhattan() == 1:
+        #         sides -= 1
         res += sides
     return res
 
@@ -35,32 +63,72 @@ def test_solve_part1():
     data = parse(fileinput.input(TEST_FILE))
     assert solve_part1(data) == 64
 
+def can_escape(grid: Grid3, coord: Vec3) -> bool:
+    if grid[coord]:
+        return False
+    seen = grid.clone()
+    queue = deque()
+    for c in coord.cneighbors():
+        if min(c) < 0 or c.x >= grid.max_x or c.y >= grid.max_y or c.z >= grid.max_z:
+            return True
+        if not seen[c]:
+            queue.append(c)
+    while queue:
+        cur = queue.popleft()
+        if seen[cur]:
+            continue
+        seen[cur] = True
+        for c in cur.cneighbors():
+            if min(c) < 0 or c.x >= grid.max_x or c.y >= grid.max_y or c.z >= grid.max_z:
+                return True
+            if not seen[c]:
+                queue.append(c)
+    return False
+
+def test_can_escape():
+    # create a hollow cube
+    grid = Grid3(7, 7, 7)
+    for x in range(1, 6):
+        for y in range(1, 6):
+            grid[(x, y, 1)] = True
+            grid[(x, y, 5)] = True
+    for z in range(2, 5):
+        for x in range(1, 6):
+            grid[(x, 1, z)] = True
+            grid[(x, 5, z)] = True
+        for y in range(2, 5):
+            grid[(1, y, z)] = True
+            grid[(5, y, z)] = True
+    assert grid[(0,0,0)] == False
+    assert grid[(2,1,3)] == True
+    assert grid[(2,2,2)] == False
+    assert grid[(3,3,3)] == False
+    assert can_escape(grid, Vec3(0, 0, 0)) == True
+    assert can_escape(grid, Vec3(1, 1, 1)) == False
+    assert can_escape(grid, Vec3(2, 2, 2)) == False
+    assert can_escape(grid, Vec3(3, 3, 3)) == False
+
 def solve_part2(data):
     res = 0
-    max_x = max((n.x for n in data))
-    max_y = max((n.y for n in data))
-    max_z = max((n.z for n in data))
-    for x in range(max_x):
-        for y in range(max_y):
-            for z in range(max_z):
-                if Vec3(x, y, z) in data:
-                    continue
-                if all((n in data for n in Vec3(x, y, z).cneighbors())):
-                    print((x, y, z))
-                    res -= 6
-    for i in range(len(data)):
+    max_size = max((max(vec) for vec in data)) + 1
+    grid = Grid3(max_size, max_size, max_size)
+    for cur in data:
+        grid[cur] = True
+    for z in range(max_size):
+        for y in range(max_size):
+            for x in range(max_size):
+                c = Vec3(x, y, z)
+                if not grid[c] and not can_escape(grid, c):
+                    grid[c] = True
+                    #print(c)
+    for cur in data:
         sides = 6
-        x1, y1, z1 = data[i]
-        for j in range(len(data)):
-            if i == j:
-                continue
-            x2, y2, z2 = data[j]
-            if x1 == x2 and y1 == y2 and abs(z1 - z2) == 1:
+        for other in cur.cneighbors():
+            if max(other) < max_size and grid[other]:
                 sides -= 1
-            elif abs(x1 - x2) == 1 and y1 == y2 and z1 == z2:
-                sides -= 1
-            elif x1 == x2 and abs(y1 - y2) == 1 and z1 == z2:
-                sides -= 1
+        # for other in data:
+        #     if other != cur and abs(cur - other).manhattan() == 1:
+        #         sides -= 1
         res += sides
     return res
 
